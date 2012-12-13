874d14a0558e7b2c6f92c59046e0c5eb550f2ec1
Argel Arias <levhita@gmail.com>
2012-12-13 00:36:02 -0600

    logo

--- public/js/room.js
+++ public/js/room.js
@@ -120,18 +120,22 @@
 			var escapedName = message.user.username.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 			var date = new Date(message.time * 1000);
 			var date = utils.getLocaleShortDateString(date) + " " + date.toLocaleTimeString();
-			
+			var string = '';
 			if(message.user.type=='TWITTER') {
-				$('#messages').prepend('<div class="message" id="'+message.id+'"><div class="info"><span class="user">' + escapedName + '</span> <a class="muted" target="_BLANK" href="http://twitter.com/'+message.user.username+'">(@'+message.user.username+')</a><div class="time">' + date + '</div></div><div class="text">' + utils.markdown(message.text) +"<div>"+ previewsHTML+'</div>');	
+				string = '<div class="message" id="'+message.id+'"><div class="info"><span class="user">' + escapedName + '</span> <a class="muted" target="_BLANK" href="http://twitter.com/'+message.user.username+'">(@'+message.user.username+')</a><div class="time">' + date + '</div></div><div class="text">' + utils.markdown(message.text) +"</div>";	
 			} else if(message.user.type=='OFFICIAL') {
-				$('#messages').prepend('<div class="message" id="'+message.id+'"><div class="info"><span class="user">' + escapedName + '</span> <span class="muted">(Official)</span><div class="time">' + date + '</div></div><div class="text">' + utils.markdown(message.text) +'</div>'+ previewsHTML+'</div>');
+				string = '<div class="message" id="'+message.id+'"><div class="info"><span class="user">' + escapedName + '</span> <span class="muted">(Official)</span><div class="time">' + date + '</div></div><div class="text">' + utils.markdown(message.text) +'</div>';
 			} else if(message.user.type=='SYSTEM') {
 				var escapedText = message.text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-				$('#messages').prepend('<div class="message system" id="'+message.id+'"><span class="time">' + date + ':</span> ' + escapedText +'</div>');
+				string = '<div class="message system" id="'+message.id+'"><span class="time">' + date + ':</span> ' + escapedText +'</div>';
 			} else {
-				$('#messages').prepend('<div class="message" id="'+message.id+'"><div class="info"><span class="user">' + escapedName + '</span> <span class="muted">(Anonymous)</span><div class="time">' + date + '</div></div><div class="text">' + utils.markdown(message.text) +'</div>'+ previewsHTML+'</div>');
+				string = '<div class="message" id="'+message.id+'"><div class="info"><span class="user">' + escapedName + '</span> <span class="muted">(Anonymous)</span><div class="time">' + date + '</div></div><div class="text">' + utils.markdown(message.text) +'</div>';
 			}
-			
+			if ( message.user.type!='SYSTEM') {
+				$('#messages').prepend(string);
+			} else {
+				$('#messages').prepend(string + previewsHTML+'</div>');
+			}
 		}
 
 		self.renderUser = function (user) {
